c45a0b687a371bb4d1a68341ab3dab549fe3b732
willfruttero <willfruttero@outlook.com>
2022-02-04 15:52:29 -0300

    Project created

---
+++ src/test/java/cursojavacompleto/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package cursojavacompleto.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
