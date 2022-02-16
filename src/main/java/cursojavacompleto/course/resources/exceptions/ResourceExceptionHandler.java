a0ae4905afbc17071447334dddf85b398602faa0
willfruttero <willfruttero@outlook.com>
2022-02-16 16:30:11 -0300

    Exception handling - findById

---
+++ src/main/java/cursojavacompleto/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package cursojavacompleto.course.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import cursojavacompleto.course.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest resquest) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				resquest.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
--- src/main/java/cursojavacompleto/course/services/UserService.java
+++ src/main/java/cursojavacompleto/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import cursojavacompleto.course.entities.User;
 import cursojavacompleto.course.repositories.UserRepository;
+import cursojavacompleto.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 	Optional<User> obj = repository.findById(id);
-	return obj.get();
+	return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
